5c6f719979a767b615c5fce0bf4abdaaf5024689
fahmisyarif97 <fahmisyarief97@gmail.com>
2017-05-03 13:54:35 +0700

    belajar require dan include

---
+++ Fahmi.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Fahmi
+{
+public $nama = "Fahmi";
+}
+
+
+
+
+ ?>
---
+++ belajar/table.php
@@ -0,0 +1,12 @@
+<table border="1">
+	<tr>
+		<td>No</td>
+		<td>nim</td>
+		<td>nama</td>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>77</td>
+		<td>sarap</td>
+	</tr>
+</table>
